3f87a7c2d9b2016c29c9dd9d87c50416e866884c
yalanskyyalexey <yalanskyyalexey@gmail.com>
2021-07-23 12:52:49 +0000

    Deploying to gh-pages from @ yalanskyyalexey/team-project-ice@5300a070e8e0b1f2eb45b4b938ebccb0f9a2f6c4 🚀

--- src.3c508b21.js
+++ src.3c508b21.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-project-ice/src.ce744ec5.js.map
+//# sourceMappingURL=/team-project-ice/src.3c508b21.js.map
