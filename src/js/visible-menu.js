38990d328e045b3c55f6c7f3019f16d11c28d689
Dmitriy Shadurskiy <dnbdimka@gmail.com>
2021-07-26 14:02:31 +0300

    done desktop version

---
+++ src/js/visible-menu.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $(".js").click(function(){
+        $(".js").hide();
+        $("ul>li>ul").hide();
+    });
+	
+	
+	
+    $("ul>li:nth-child(3)").click(function(){
+        $(".js").show();
+        $("ul>li>ul").show();
+    });
+});
