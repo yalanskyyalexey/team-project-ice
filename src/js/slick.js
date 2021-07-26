0d44678d3e656c6b5abc8030b199eddfcbe89297
Evgeniy-the-Great <shalenkoevgeniy@gmail.com>
2021-07-26 15:20:04 +0300

    add slick on contack section

--- src/js/slick.js
+++ src/js/slick.js
@@ -1,1 +1,20 @@
 // Настройки для слайдера
+$('.contact-list').slick({
+  autoplay: true,
+  speed: 1500,
+  arrows: false,
+  dots: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+
+  responsive: [
+    {
+      breakpoint: 4000,
+      settings: 'unslick',
+    },
+    {
+      breakpoint: 768,
+      settings: 'slick',
+    },
+  ],
+});
