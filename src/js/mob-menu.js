967785aa5edf398bda844225c81e2d0ed483282b
Dmitriy Shadurskiy <dnbdimka@gmail.com>
2021-07-27 15:43:33 +0300

    commit

--- src/js/mob-menu.js
+++ src/js/mob-menu.js
@@ -2,16 +2,37 @@
   const refs = {
     openModalBtn: document.querySelector('[data-menu-open]'),
     closeModalBtn: document.querySelector('[data-menu-close]'),
+    closeModalLink: document.querySelector('[data-menu-link-close]'),
+    closeModalMenuBtn: document.querySelector('[data-menu-btn-close]'),
+
     modal: document.querySelector('[data-menu]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalLink.addEventListener('click', toggleModal);
+  refs.closeModalMenuBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-open');
+  }
+})();
+// confirm order
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[header-modal-open]'),
+    closeModalBtn: document.querySelector('[header-modal-close]'),
+    modal: document.querySelector('[header-data-modal]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle('is-open');
+    refs.modal.classList.toggle('is-hidden');
   }
 })();
+
 //about section modal
 (() => {
   const refs = {
