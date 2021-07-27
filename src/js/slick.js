9a03c7d66b2d186a3206976e81615c7c39338daa
Anatolii Bairov <tolikbairov16@gmail.com>
2021-07-27 16:06:36 +0300

    add slider to customer-review section

--- src/js/slick.js
+++ src/js/slick.js
@@ -1,1 +1,17 @@
 // Настройки для слайдера
+
+
+// Настройки для слайдера customer_review
+$('.review__slider-track').slick({
+   arrows: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  adaptiveHeight: true,
+  /* variableWidth: true, */
+  infinite: false,
+  autoplay: true,
+  autoplaySpeed: 4000,
+  dots: true,
+  dotsClass: "customer-reviews-dots"
+  
+ });
