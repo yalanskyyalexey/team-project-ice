d272c36948682d617e97b3061d4e4cc2c7f210f2
Yalanskyy <yalanskyyalexey@gmail.com>
2021-07-28 00:50:13 +0300

    Update about-modal.js

--- src/js/about-modal.js
+++ src/js/about-modal.js
@@ -5,8 +5,8 @@
     modal: document.querySelector('[about-modal-menu]'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openAboutModalBtn.addEventListener('click', toggleModal);
+  refs.closeAboutModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidd');
