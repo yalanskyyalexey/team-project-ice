5eae47f61ef5e861bc34ebd243526e596d10f75a
Dmitriy Shadurskiy <dnbdimka@gmail.com>
2021-07-27 13:41:53 +0300

    add effects

--- src/js/visible-menu.js
+++ src/js/visible-menu.js
@@ -1,13 +1,13 @@
-$(document).ready(function(){
-    $(".js").click(function(){
-        $(".js").hide();
-        $("ul>li>ul").hide();
-    });
+// $(document).ready(function(){
+//     $(".js").click(function(){
+//         $(".js").hide();
+//         $("ul>li>ul").hide();
+//     });
 	
 	
 	
-    $("ul>li:nth-child(3)").click(function(){
-        $(".js").show();
-        $("ul>li>ul").show();
-    });
-});
+//     $("ul>li:nth-child(3)").click(function(){
+//         $(".js").show();
+//         $("ul>li>ul").show();
+//     });
+// });
