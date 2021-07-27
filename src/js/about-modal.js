b6c2fed8f07f13591a339d80a8da69bc9a14d80d
Yalanskyy <yalanskyyalexey@gmail.com>
2021-07-28 00:19:50 +0300

    add fix modal

--- src/js/about-modal.js
+++ src/js/about-modal.js
@@ -2,13 +2,13 @@
   const refs = {
     openAboutModalBtn: document.querySelector('[about-modal-open]'),
     closeAboutModalBtn: document.querySelector('[about-modal-close]'),
-    modalMenuAbout: document.querySelector('[about-modal-menu]'),
+    modal: document.querySelector('[about-modal-menu]'),
   };
 
   refs.openAboutModalBtn.addEventListener('click', toggleModal);
   refs.closeAboutModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modalMenuAbout.classList.toggle('is-hidd');
+    refs.modal.classList.toggle('is-hidd');
   }
 })();
